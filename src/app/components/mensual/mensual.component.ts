a5f6b463103cdb2b2bb636a11af9ffccd16d7f85
MJoDev <mjzdo2020@gmail.com>
2024-01-18 15:20:40 -0400

    MENSUAL
    casi listo

--- src/app/components/mensual/mensual.component.ts
+++ src/app/components/mensual/mensual.component.ts
@@ -14,12 +14,11 @@ import moment from 'moment';
 export class MensualComponent implements OnInit{
 
     usuarios: any[] = [];
-    fechasSemana: Date[] = this.obtenerFechas(); // Ahora inicializamos fechasSemana aquí
-    momentfechas: string[] = this.cambiarFechas();
+    momentfechas: string[] = this.getDatesOfMonth();
     fechaYear: string = "";
     filtroNombre: string = '';
-    NombreDiasSemana: string[] = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
     datesOfMonthWithDay: { date: Date; dayOfWeek: string }[] = this.getDatesOfMonthWithDay();
+  
 
     constructor(
     private router: Router, 
@@ -28,9 +27,10 @@ export class MensualComponent implements OnInit{
     ){}
     ngOnInit(): void {
       this.obtenerUsuarios();
-      console.log(this.momentfechas);
       const fechaActual = new Date();
       this.fechaYear = moment(fechaActual).format('YYYY-MM-DD');
+
+      console.log(this.momentfechas);
     }
     obtenerUsuarios(){
       this.usuarioService.obtenerUsuarios().subscribe(
@@ -53,48 +53,32 @@ export class MensualComponent implements OnInit{
         }
       );
     }
-    obtenerFechas(): Date[] {
-      const fechaActual = new Date();
-      const diaSemana = fechaActual.getDay();
-      const primerDiaSemana = new Date(fechaActual);
-      primerDiaSemana.setDate(fechaActual.getDate() - diaSemana + (diaSemana === 0 ? -6 : 1));
-      
-  
-      const fechasSemana = [];
-      const fechasFormateadas = [];
-      for (let i = 0; i < 7; i++) {
-        const nuevaFecha = new Date(primerDiaSemana);
-        nuevaFecha.setDate(primerDiaSemana.getDate() + i);
-  
-        fechasSemana.push(nuevaFecha);
-    }
-  
-      return fechasSemana;
-    }
-    cambiarFechas(): string[] {
-      //primero obtiene la fecha actual, el dia de hoy
-      const fechaActual = new Date();
-      //luego, obtiene el valor del dia de la semana  para domingo, 6 para sabado
-      const diaSemana = fechaActual.getDay();
-      //el primer dia de la semana es igual a la fecha actual
-      const primerDiaSemana = new Date(fechaActual);
-      //el primer dia de la semana es igual a el valor - el valor
-      primerDiaSemana.setDate(fechaActual.getDate() - diaSemana + (diaSemana === 0 ? -6 : 1));
-      
-  
+    getDatesOfMonth(): string[] {
+      const currentDate = new Date();
+      const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+      const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+    
+      const datesArray: Date[] = [];
+      let currentDatePointer = firstDayOfMonth;
+    
       const momentfechas = [];
-      const fechasFormateadas = [];
-      for (let i = 0; i < 7; i++) {
-        const nuevaFecha = new Date(primerDiaSemana);
-        nuevaFecha.setDate(primerDiaSemana.getDate() + i);
-        var localTime = moment(nuevaFecha).format('YYYY-MM-DD');
-        var proposedDate = localTime + "T00:00:00.000Z";
-  
-        momentfechas.push(proposedDate);
+
+      while (currentDatePointer <= lastDayOfMonth) {
+        datesArray.push(new Date(currentDatePointer));
+        currentDatePointer.setDate(currentDatePointer.getDate() + 1);
+        
       }
-  
+
+      for(var i = 0; i < datesArray.length; i++){
+        const nuevaFecha = moment(datesArray[i]).format('YYYY-MM-DD');
+        var proposedDate = nuevaFecha + "T00:00:00.000Z";
+        momentfechas.push(proposedDate);
+
+      }
+
       return momentfechas;
     }
+
     imprimirLista(){
       // Extraemos el
       const DATA = document.getElementById('listaAnual')!;
@@ -119,6 +103,7 @@ export class MensualComponent implements OnInit{
         docResult.save(`${new Date().toISOString()}.pdf`);
       });
     }
+
     getDatesOfMonthWithDay(): { date: Date; dayOfWeek: string }[] {
       const currentDate = new Date();
       const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
